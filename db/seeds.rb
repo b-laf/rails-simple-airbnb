0bf0efd928daf702f595809c07727aec59d557d3
Bastien Lafont <lafont.bastien@gmail.com>
2023-02-22 17:34:57 +0100

    end of unopt simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+Flat.destroy_all
+
+15.times do
+  Flat.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(30..200),
+    number_of_guests: rand(1..10),
+    urlimg: "https://picsum.photos/500/#{rand(300..400)}"
+  )
+end
